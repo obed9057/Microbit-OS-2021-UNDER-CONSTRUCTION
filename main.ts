29adae8d4e3f9e467747d94fa879a80a44460712
obed9057 <66080119+obed9057@users.noreply.github.com>
2021-08-04 20:51:31 -0400

    Added web recovery system

--- main.ts
+++ main.ts
@@ -45,6 +45,62 @@ input.onButtonPressed(Button.B, function () {
     basic.pause(5000)
     basic.showString("You cannot use the *Prompt* Because updates are still being worked on by the Full Power Tech Developer Team Please try again later Press A+B To go Home.")
     basic.clearScreen()
+    basic.showLeds(`
+        # . . . #
+        . # # # .
+        . . # # .
+        . . . # .
+        . . . . #
+        `)
+    basic.pause(5000)
+    basic.showString("MIO3.SYS Took to long to respond please restart to repair MI03 as it is an important system file.")
+    basic.pause(5000)
+    basic.clearScreen()
+    basic.showString("ERROR COULD NOT REPAIR DUE TO A MISSING FILE THE FILE IS LOCATED AT MI03/SYS/BOOT/REPAIR")
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        # . # . #
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(5000)
+    basic.clearScreen()
+    basic.showString("Welcome to WEB BOOT RECOVERY ROOTED AT MI01/MI03 NOT FOUND/WEB")
+    basic.pause(5000)
+    basic.showString("You have been sent here for the folowing reason..")
+    basic.pause(5000)
+    basic.showString("//,MI03 HAS BEEN DELETED BOOTING FROM MI01 EARLY RELEASE")
+    basic.pause(5000)
+    basic.showString("You might be wondering what is MI01 ISNT IT SUPPOSED TO BE MI03")
+    basic.pause(5000)
+    basic.showString("MIO1 IS THE WEBOS REPLACEMENT FOR MI03 YOU CAN ONLY GET HERE IF YOUR SYSTEM HAS BEEN DELETED")
+    basic.pause(5000)
+    basic.showString("DONT WORRY MI03 BACKS UP EVERYTIME YOU TURN OFF OR RESTART YOUR SYSTEM PICK A MI03 BUILD TO COPY TO MI101 BOOT FILES")
+    basic.pause(5000)
+    basic.showString("MI101 IS THE BOOT MANAGER FOR MI01 WE WILL RECOVER AS MUCH FILES AS WE CAN")
+    basic.pause(5000)
+    basic.showString("SO YOU CAN HAVE YOUR APPS ON MI01 IT CAN TAKE UP TO A WEEK FOR US TO GET YOU YOUR MI03 BACK")
+    basic.pause(5000)
+    basic.showString("for now you will have to use MI01")
+    basic.pause(5000)
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # . # . #
+        # # . # #
+        # . . . #
+        `)
+    basic.pause(5000)
+    basic.showLeds(`
+        . . . # #
+        . . # # #
+        . # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(5000)
+    basic.showString("cracking code absorbing power copying code uploading")
 })
 basic.showLeds(`
     # # . # #
